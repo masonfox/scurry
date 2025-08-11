aaae3abfd5624d919120b4f318b0d805a1a848f7
Mason Fox <masonfox22@gmail.com>
2025-08-11 09:32:20 -0400

    add fav/icons

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,11 @@
 export const metadata = {
   title: "Scurry",
-  description: "A nimble little mouse that scurries through MyAnonamouse (MAM) and whisks torrents into qBittorrent"
+  description: "A nimble little mouse that scurries through MyAnonamouse (MAM) and whisks torrents into qBittorrent",
+  icons: {
+    icon: '/favicon.ico',
+    shortcut: '/apple-icon.png',
+    apple: '/apple-icon.png',
+  }
 };
 
 import "./styles/globals.css";
