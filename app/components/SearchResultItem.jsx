aa87d49a8bd08eed356de40290e5046cf151f988
Mason Fox <masonfox@users.noreply.github.com>
2026-09-02 11:10:31 -0400

    fix "by" author margin issue

--- app/components/SearchResultItem.jsx
+++ app/components/SearchResultItem.jsx
@@ -62,9 +62,9 @@ export default function SearchResultItem({ result, onAddItem, selectable = false
                   />
                 </span>
               )}
-              <span>{result.title}</span>
+              <span className="mr-1">{result.title}</span>
               <span className="text-gray-600 dark:text-zinc-400 font-normal">
-                <span className="mx-1">by</span>
+                <span className="mr-1">by</span>
                 <span>{result.author}</span>
                 {result.narrator && <span> ({result.narrator})</span>}
               </span>
